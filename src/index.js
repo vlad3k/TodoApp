0f8e032ed5f21bc43ff2183bb55107de9a99c818
vlad3k <vlad3k@bk.ru>
2019-08-20 20:25:35 +0500

    Appearance

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('Hello, world!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/app';
+
+ReactDOM.render(<App />,
+  document.getElementById('root'));
