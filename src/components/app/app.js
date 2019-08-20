6a12a6e500f92de1786ebbfa417965ebc3447251
vlad3k <vlad3k@bk.ru>
2019-08-20 21:59:12 +0500

    add events like important, done and remove element

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
@@ -7,14 +7,33 @@ import ItemStatusFilter from '../item-status-filter';
 
 import './app.css';
 
-const App = () => {
+export default class App extends Component {
 
-    const todoData = [
+  state = {
+    todoData: [
       { label: 'Drink Coffee', important: false, id: 1 },
       { label: 'Make Awesome App', important: true, id: 2 },
       { label: 'Have a lunch', important: false, id: 3 }
-    ];
+    ],
+  }
+
+  deleteItem = (id) => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((el) => el.id === id)
+
+      const newArray = [
+        ...todoData.slice(0, idx),
+        ...todoData.slice(idx + 1)
+      ];
+
+      return {
+        todoData: newArray
+      }
+    })
+  };
   
+  render() {
+    const { todoData } = this.state;
     return (
       <div className="todo-app">
         <AppHeader toDo={1} done={3} />
@@ -23,9 +42,10 @@ const App = () => {
           <ItemStatusFilter />
         </div>
   
-        <TodoList todos={todoData} />
+        <TodoList 
+          todos={todoData}
+          onDeleted={ this.deleteItem } />
       </div>
     );
-  };
-
-  export default App;
+  } 
+};
